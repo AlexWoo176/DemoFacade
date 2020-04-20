ed31542148a5dfd349e9841b27442dbb03387758
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-04-20 15:48:03 +0700

    Demo Facade

---
+++ src/com/codegym/Linkedln.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class Linkedln implements SocialShare {
+    private String message;
+
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void share() {
+        System.out.println("Sharing to Linkedln: " + this.message);
+    }
+}
